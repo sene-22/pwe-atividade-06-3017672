d03d33a78fae1c72d070f78d83a371be443a4ac1
Vinicius Santos <viniciusalvesrochadosantos@gmail.com>
2024-11-05 22:02:15 +0000

    update

---
+++ Questao_03.js
@@ -0,0 +1,11 @@
+const nome = "João da Silva"
+const primeira_nota = 7
+const segunda_nota = 8.5
+
+const mult_primeira_nota = primeira_nota * 0.6
+const mult_segunda_nota = segunda_nota * 0.4
+
+const total = mult_primeira_nota + mult_segunda_nota
+
+window.alert(`Nome do aluno: ${nome} \nNota 01: ${primeira_nota} \nNota 02: ${segunda_nota}`)
+window.alert(`Nome do aluno: ${nome} \nNota Final: ${total}`)
